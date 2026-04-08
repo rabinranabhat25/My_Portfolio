93a2bb70ece06936a0b065b828122e05e42a477d
Rabin Ranabhat <rawbinranabhat9876@gmail.com>
2026-04-08 15:27:24 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './component/Hero'
 import About from './component/About'
 import Skills from './component/Skills'
 import Project from './component/Project'
+import Contact from './component/Contact'
 
 
 const App = () => {
@@ -37,6 +38,7 @@ const toggleDarkMode =()=>{
       <About darkMode={darkMode}/>
       <Skills darkMode={darkMode}/>
       <Project darkMode={darkMode}/>
+      <Contact darkMode={darkMode}/>
     </div>
   )
 }
